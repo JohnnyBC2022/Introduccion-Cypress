94651bb5adcf8b90f2bc26830ebe3521d62c14b5
Johnny <jonathanbc2012@gmail.com>
2025-03-13 21:13:23 +0100

    first commit

---
+++ cypress/e2e/sección_01/hola_mundo.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+describe('Primer test con Cypress', () => {
+  it('Esto es un hola mundo desde Cypress', ()=> {
+    cy.log('Hola, bienvenido a Cypress')
+    cy.visit('https://play2.automationcamp.ir/')
+
+    cy.wait(2000)
+    cy.get('#fname').type('Jonathan')
+    cy.get('#lname').type('Cypress')
+  })
+
+
+})
---
+++ cypress/e2e/sección_01/validando_titulo.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe('Sección 1 Validando el título  ', () => {
+  it('Test Validar el título de la página ', ()=> {
+    cy.visit('https://play2.automationcamp.ir/')
+    cy.title().should('eq','Try Testing This')
+
+    cy.log('Ok, la función title funcionó bien')
+  })
+
+
+})
