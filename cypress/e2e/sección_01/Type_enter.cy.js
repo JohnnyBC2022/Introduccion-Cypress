5fd300d7326f5c9bf1c421c3e8c2c5cbe99483bb
Johnny <jonathanbc2012@gmail.com>
2025-03-23 13:20:07 +0100

    Test(cfg) type enter added

---
+++ cypress/e2e/sección_01/Type_enter.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+describe('Funciones para type', () => {
+  it('Type --> enter ', ()=> {
+    cy.visit('https://www.google.es/')
+    cy.title().should('eq', 'Google')
+    cy.wait(1500)
+    cy.get('[name="q"]').type('cypress io {enter}')
+    cy.wait(1000)
+    cy.get('#rso > div.hlcw0c > div > div > div > div > div > div > div > div:nth-child(1) > div:nth-child(2) > div > div > span > a > h3').click()
+  })
+
+})
