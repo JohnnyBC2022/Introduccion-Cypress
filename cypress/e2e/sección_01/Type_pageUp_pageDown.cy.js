50b8b8f2cea9b63947c717c762eed45cca1ea55d
Johnny <jonathanbc2012@gmail.com>
2025-03-26 20:24:05 +0100

    Test(update) type pageUp pageDown added

---
+++ cypress/e2e/sección_01/Type_pageUp_pageDown.cy.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+describe("Ejemplo de Type pageUp, pageDown  ", () => {
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    return false;
+  })
+  it("Type pageUp ", () => {
+    cy.visit("https://demoqa.com/text-box");
+    cy.title().should("eq", "DEMOQA");
+    cy.wait(1000);
+
+    cy.get('#userName').type('{pageup}')
+    cy.wait(2000)
+  });
+
+  it.only("Type pageDown ", () => {
+    cy.visit("https://demoqa.com/text-box");
+    cy.title().should("eq", "DEMOQA");
+    cy.wait(1000);
+
+    cy.get('#userName').type('{pagedown}')
+    cy.wait(2000)
+  });
+});
